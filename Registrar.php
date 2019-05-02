d016442e227ac772704ffb4fede153b6200e485f
Edgar Martinez <agustin__mtz00@outlook.com>
2019-05-01 23:02:41 -0500

    Regresa id al registrar

--- Registrar.php
+++ Registrar.php
@@ -4,17 +4,24 @@ include 'ConexionDB.php';
 $inputJSON = file_get_contents('php://input');
 $input = json_decode($inputJSON, TRUE);
 $insertQuery = "INSERT INTO usuario (correo, contrasena, peso, estatura, edad) VALUES (:correo, :pwd, :peso, :estatura, :edad)";
-$response = new stdClass();
+$query = "Select ID from usuario where Correo = :correo and Contrasena = :pwd";
 
-if(isset($input['correo']) && isset($input['pwd']) && isset($input['peso']) && isset($input['estatura']) && isset($input['edad'])){
+
+if(isset($input['correo']) && isset($input['contrasena']) && isset($input['peso']) && isset($input['estatura']) && isset($input['edad'])){
 	$stmt = $dbConn->prepare($insertQuery);
 		$stmt->bindparam(':correo',$input['correo']);
-        $stmt->bindparam(':pwd',$input['pwd']);
+        $stmt->bindparam(':pwd',$input['contrasena']);
         $stmt->bindparam(':peso',$input['peso']);
         $stmt->bindparam(':estatura',$input['estatura']);
         $stmt->bindparam(':edad',$input['edad']);
         $stmt->execute();
+    $stmt = $dbConn->prepare($query);
+        $stmt->bindparam(':correo',$input['correo']);
+        $stmt->bindparam(':pwd',$input['contrasena']);
+        $stmt->execute();
+        $result = $stmt->fetch();
     $response['msg'] = "exito";
+    $response['id'] = $result['ID'];
 }else{
     $response['msg'] = "Fallo";
 }
