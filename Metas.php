97c1efdb494795e80fdc583388f2e7ccf3bc9fa1
EdgarMtz00 <edgar.agustin.martinez@gmail.com>
2019-05-28 01:11:27 -0500

    avances a unas horas de entregar

--- Metas.php
+++ Metas.php
@@ -2,7 +2,7 @@
 include "ConexionDB.php";
 
 function post(PDO $dbConn, $input){
-    $insertMeta = "Update usuario set meta = :meta, promedio = 0, record = 0, ultimo = 0, contador = 0 where id = :id";
+    $insertMeta = "Update usuario set meta = :meta, promedio = 0, record = 0, ultimo = 0, contador = 0, total = 0 where id = :id";
     $stmt = $dbConn->prepare($insertMeta);
     $stmt->bindParam(':meta',$input['meta']);
     $stmt->bindParam(':id', $input{'idUsuario'});
@@ -19,12 +19,28 @@ function get(PDO $dbConn, $idUsuario){
 }
 
 function put(PDO $dbConn, $input){
-    $query = "Select record, promedio, contador from usuario where ID = :id";
+    $query = "Select record, promedio, contador, total from usuario where ID = :id";
     $stmt = $dbConn->prepare($query);
     $stmt->bindParam(":id", $input['idUsuario']);
     $stmt->execute();
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
-
+    if($input['repeticiones'] > $result['record']){
+        $updateRecord = ", record = " . $input['repeticiones'];
+    }else{
+        $updateRecord = "";
+    }
+    $result['contador']++;
+    $result['total'] += $input['repeticiones'];
+    $promedio = $result['total'] / $result['contador'];
+    $update = "Update usuario set total = :t, ultimo = :u, promedio = :p, contador = :c" . $updateRecord . " where ID = :id";
+    $stmt = $dbConn->prepare($update);
+    $stmt->bindParam(':u', $input['repeticiones']);
+    $stmt->bindParam(':p', $promedio);
+    $stmt->bindParam(':c', $result['contador']);
+    $stmt->bindParam(':id', $input['idUsuario']);
+    $stmt->bindParam(':t', $result['total']);
+    $stmt->execute();
+    return '{"mensaje": "ok"}';
 }
 
 $inputJSON = file_get_contents('php://input');
